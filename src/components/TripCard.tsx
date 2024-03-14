05b739e09ab22bd6552bcb6380721481ec516f64
Alex Jamarco <a.jamarco@gmail.com>
2024-03-14 21:41:44 +0000

    needed fix

--- src/components/TripCard.tsx
+++ src/components/TripCard.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 //import image from src\assets\hotel-image-1.png
 import image1 from "../assets/hotel-image-1.png";
 
@@ -5,17 +7,41 @@ import image1 from "../assets/hotel-image-1.png";
 import "../styles/TripCard.css";
 
 const TripCard = () => {
+  const [showOverview, setShowOverview] = useState(false);
   return (
     <div className="trip_card">
       <section className="hotel_info">
-        <img src={image1} alt="hotel" />
-        <h2>Hotel Name</h2>
-        <p>Price</p>
-        <p>Star Rating</p>
+        <div
+          className="hotel_image"
+          style={{ backgroundImage: `url(${image1})` }}
+        >
+          <button
+            className="button_more_info"
+            onClick={() => setShowOverview(!showOverview)}
+          >
+            <b>{showOverview ? "Read less" : "Read more"}</b> about this hotel
+          </button>
+        </div>{" "}
+        <div className="hotel_details">
+          <h2>IberoStar</h2>
+          <p>Tenerife</p>
+          <p>Star Rating</p>
+          <div className="hotel_more_info">
+            <p>2 Adults, 2 Children & 1 infant</p>
+            <p>3rd July 2019 for 7 days</p>
+            <p>Departing from East Midlands</p>
+          </div>
+          <button className="button_book">
+            <p>Book now</p>
+            <h1>£ 3,123.25</h1>
+          </button>
+        </div>
       </section>
-      <section className="hotel_overview">
+      <section
+        className={showOverview ? "hotel_overview" : "hotel_overview hide"}
+      >
         <h3>Overview</h3>
-        <p>
+        <p className="hotel_overview_details">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
           malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum.
           Donec mollis hendrerit risus eu posuere.
